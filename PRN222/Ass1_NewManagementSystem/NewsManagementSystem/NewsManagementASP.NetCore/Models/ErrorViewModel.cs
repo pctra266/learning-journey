347773ea95cec3aa392e48e0abadeb1a3e5934ca
Tra Pham <phamtra001@gmail.com>
2025-05-20 15:20:42 +0700

    feat: add new folder

---
+++ PRN222/Ass1_NewManagementSystem/NewsManagementSystem/NewsManagementASP.NetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewsManagementASP.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PRN222/Ass1_NewManagementSystem/TestHttpClient/TestHttpClient/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestHttpClient
+{
+    internal class Program
+    {
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
