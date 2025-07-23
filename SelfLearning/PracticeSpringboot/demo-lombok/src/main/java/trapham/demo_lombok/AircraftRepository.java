f2cae95f5fc87429eafb27916dc3df9764faba02
Tra Pham <phamtra001@gmail.com>
2025-07-23 19:36:16 +0700

    organize folder

---
+++ SelfLearning/PracticeSpringboot/demo-lombok/src/main/java/trapham/demo_lombok/AircraftRepository.java
@@ -0,0 +1,5 @@
+package trapham.demo_lombok;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> {}
---
+++ SelfLearning/PracticeSpringboot/demo-lombok/src/test/java/trapham/demo_lombok/DemoLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package trapham.demo_lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
