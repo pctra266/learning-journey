674b6fa568aeeea8978dde61d37e24a80e42cdb2
Tra Pham <phamtra001@gmail.com>
2025-07-24 10:53:25 +0700

    spring boot: repository-base using graphDB (Neo4j)

---
+++ SelfLearning/PracticeSpringboot/demo-graphdb/src/main/java/trapham/demo_graphdb/DemoGraphdbApplication.java
@@ -0,0 +1,13 @@
+package trapham.demo_graphdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGraphdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGraphdbApplication.class, args);
+	}
+
+}
---
+++ SelfLearning/PracticeSpringboot/demo-graphdb/src/test/java/trapham/demo_graphdb/DemoGraphdbApplicationTests.java
@@ -0,0 +1,13 @@
+package trapham.demo_graphdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
