bfd8eb0046352d3bfc3a3efeb6f023de7d8d2523
Tra Pham <phamtra001@gmail.com>
2025-08-01 22:28:17 +0700

    spring boot: demo DataJPA test

---
+++ SelfLearning/PracticeSpringboot/demo-Thymeleaf/src/test/java/trapham/demo_Thymeleaf/AircraftRepositoryTest.java
@@ -0,0 +1,19 @@
+package trapham.demo_Thymeleaf;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AircraftRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
